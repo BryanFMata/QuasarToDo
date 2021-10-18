e5d6bd83cf9f9fe467f0476a6bf26991a4d6ab81
BryanFMata <bryan.mata222@gmail.com>
2021-10-18 15:30:53 -0300

    Quasar Todo V1.0

---
+++ quasar-todo/src/components/NomeUsuario.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <h5>{{nome}}</h5>
+    <h6>{{dataHora}}</h6>
+</div>
+</template>
+
+<script>
+  export default {
+  name: 'UserName',
+  components: {},
+  props:['nome'],
+  data () {
+    return {
+        dataHora: Date.now()
+    }
+  },
+  mounted () {
+  },
+  computed: {
+  },
+  methods: {
+  },
+  watch: {
+  }
+}    
+</script>
---
+++ quasar-todo/src/pages/Help.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis pellentesque lorem. In tincidunt tortor ligula, in egestas nulla viverra nec. Nunc fermentum, justo ut vestibulum dictum, nunc lorem mollis quam, mattis egestas turpis arcu in magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi posuere turpis sit amet augue facilisis, ut eleifend tortor sagittis. Morbi a purus et dui convallis malesuada et sed ipsum. Nullam nisl ex, scelerisque ac purus eu, pellentesque dictum augue. Donec porttitor elit at nisi mattis efficitur. Aenean ut metus nisi. Praesent pulvinar purus eros, vel condimentum ex ultricies sed. Vestibulum velit lorem, fermentum et tempus sit amet, rutrum quis nulla. Ut vitae nibh accumsan, dapibus tellus ac, facilisis dui. Vestibulum vitae tellus pellentesque, feugiat magna quis, pulvinar dui. Duis ut lacus quis ipsum semper mollis vel a massa. Pellentesque ullamcorper egestas ligula.</p>
+  </q-page>
+</template>
+
+<script>
+ 
+</script>
+
